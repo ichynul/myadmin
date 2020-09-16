89967f5f2ce5fba349459b74e527547fa52d2c30
ichynul <ichynul@163.com>
2020-09-16 14:27:43 +0800

    update

--- application/admin/controller/Demo.php
+++ application/admin/controller/Demo.php
@@ -96,23 +96,21 @@ class Demo extends Controller
     public function demo3()
     {
         $builder = Builder::getInstance('builer', 'content');
-
-        $builder->row()->content(6)->display('<div style="widht:100%;height:30px;margin-top:10px;border:1px dashed red;">col-md-6</div>');
+        $builder->content(4)->display('<div style="widht:100%;height:30px;margin-top:10px;border:1px dashed red;">{$name}</div>', ['name' => 'col-md-4']);
         //新的一个row,所以这个col-md-6不会跟随上面一个
-        $builder->row()->content(6)->fetch(); //模板路径规则跟tp框架一致。留空模板名就是当前action`admin/view/demo/demo3.html`
-        //新的一个row,所以这个col-md-6不会跟随上面一个
-        $builder->row();
-
-        // $builder->column(6)->content() 跟 $builder->content(6)等效。table,form也类似
-        $builder->content(6)->display('<div style="widht:100%;height:30px;margin:20px 0;border:1px dashed red;">{$name}</div>', ['name' => 'col-md-6']);
-        $builder->column(4)->content()->fetch('demo33', ['name' => 'col-md-4']); //另外指定模板`admin/view/demo/demo33.html`
+        $builder->row()->column(6)->content()->display('<div style="widht:100%;height:30px;margin:20px 0;border:1px dashed red;">col-md-6，时间' . date('Y-m-d H:i:s') . '</div>');
 
         $builder->row();
 
         //树形结构，col-md-n 控制宽带在这里是不灵活的，让它变成`col-md-0 left-tree`。然后给`left-tree`定义宽度。
         $tree = $builder->tree('0 left-tree');
-        $table = $builder->table('0 right-list');
+        $right = $builder->column('0 right-list')->row();
 
+        //$row->column(4)->content() 跟 $row->content(4)等效。table,form也类似
+        $right->column(4)->content()->fetch(); //模板路径规则跟tp框架一致。留空模板名就是当前action`admin/view/demo/demo3.html`
+        $right->content(3)->fetch('demo33', ['name' => 'col-md-4', 'data' => json_encode(['colors' => ['红色', '蓝色', '橙色'], 'datas' => [300, 50, 100]])]); //另外指定模板`admin/view/demo/demo33.html`
+
+        $table = $right->table();
         $sarch = $table->getSearch();
 
         $treeData = $this->getTree();
@@ -130,7 +128,7 @@ class Demo extends Controller
         $builder->addStyleSheet('
             .left-tree
             {
-                width:12%;
+                width:11%;
                 float:left;
             }
 
@@ -141,13 +139,23 @@ class Demo extends Controller
             }
         ');
 
+        //表格ajax的时候，只会替换table部分，若需要实时更新的数据，可使用addTop\addBottom。
+
+        $top = $table->addTop();
+        $top->addStyle('padding:20px');
+
+        $top->content(12)->display('<label class="text-center label label-default">搜索:' . json_encode(input('post.')) . '</label>');
+
+        $top->content(3)->display('<p>这里是Bottom left，时间' . date('Y-m-d H:i:s') . '</p>');
+        $top->content(3)->display('<p>这里是Bottom right</p>');
+        $top->content(4)->fetch('demo333', ['data' => json_encode(['months' => ["一月", "二月", "三月", "四月", "五月", "六月", "七月"], 'in' => [mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100)], 'out' => [mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100), mt_rand(50, 100)]])]);
+        $row = $top->column(12)->row();
+
+        $row->content(6)->display('<p>这里是新的 一个 row</p>');
+
         if (request()->isAjax()) {
-
-            //表格ajax的时候，只会替换table部分，若需要实时更新的数据，可使用addTop\addBottom。
-            $table->addTop()->content()->display('<label class="text-center label label-default">搜索:' . json_encode(input('post.')) . '</label>');
-
-            $table->addBottom()->content()->display('<script>layer.msg("搜索:category_id-' . input('post.category_id') . '")</script>');
-
+            //addBottom/addTop类似，只是一个在表格上面，一个在下面
+            $table->addBottom()->content(6)->display('<script>layer && layer.msg("搜索:category_id-' . input('post.category_id') . '")</script>'); //可以附加js,在每次表格刷新是做点什么
             return $table->partial()->render();
         }
 
